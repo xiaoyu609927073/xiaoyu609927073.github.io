b04a855b69c2635c49321489ab6aec88d580412c
xiaoyu609927073 <32596543+xiaoyu609927073@users.noreply.github.com>
2018-01-30 18:05:01 +0800

    change

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 $(function() {
-    for (i = 1; i < 7; i++) {
-        $('.skill-' + i).hover(function() {
-                $(this).children('.skill-li-div').fadeIn('slow');
-            },
-            function() {
-                $(this).children('.skill-li-div').fadeOut(10);
-            });
-    };
+
+    w = $(window).width();
+    if (w > 750) {
+        for (i = 1; i < 7; i++) {
+            $('.skill-' + i).hover(function() {
+                    $(this).children('.skill-li-div').fadeIn('slow');
+                },
+                function() {
+                    $(this).children('.skill-li-div').fadeOut(10);
+                });
+        };
+    }
+
 
 })
